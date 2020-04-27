a7d861d3c9bd57e6c9b3fd24a6c7382361d304be
bloomkail <arnaud.lambert@outlook.com>
2020-04-27 15:54:34 +0200

    blindage ouverture fichier
    blindage
    main
    cout

--- main.cpp
+++ main.cpp
@@ -1,18 +1,13 @@
-#include <allegro.h>
 #include <iostream>
 #include "Graphe.h"
 
 int main()
 {
-    std::cout<<"Quel fichier voulez-vous ouvrir ? ";
-    std::string nom;
-    std::cin>>nom;
-    Graphe piscine(nom);
-    std::cin.ignore();
-    piscine.ponderation();
-    piscine.affichage();
-    piscine.dessiner();
+    Graphe a;
+    a.ponderation();
+    a.affichage();
+    a.dessiner();
 
     return 0;
 }
-END_OF_MAIN();
+
