7807c733deea05673fe2b1c387f67663609763fc
Antoine <ahaz1701@gmail.com>
2020-04-28 21:14:02 +0200

    ca en plus

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 int main()
 {
     Graphe a;
-    /*a.ponderation();
+    a.ponderation();
     a.affichage();
-    std::vector<std::pair<int, double>> centralite_degres = a.centraliteDegre ();*/
+    std::vector<std::pair<int, double>> centralite_degres = a.centraliteDegre ();
     std::vector<double> vecteurPropre=a.vecteurPropre();
-    /*a.testConnexite();
+    a.testConnexite();
     std::vector<double> vecteurProximite=a.vecteurProximite();
     a.intermediarite();
-    a.dessiner();*/
+    a.dessiner();
     return 0;
 }
